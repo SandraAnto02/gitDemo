da41b5d86593d16535c9f496fd8368e8ef7f8e18
SandraAnto02 <sandra.a@prodapt.com>
2023-10-16 12:38:54 +0530

    Created a git component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GitcomponentComponent } from './gitcomponent/gitcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitcomponentComponent
   ],
   imports: [
     BrowserModule
